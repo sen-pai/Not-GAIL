77e510493e91148aee06c34b7b00a431db9cd2bb
sen-pai <sharan_spai@protonmail.com>
2021-03-18 15:05:08 +0530

    mostly working anything

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Not-GAIL
 To install each library here use ``pip install -e``
 
+Make sure you are not using python 3.9, I faced many installation problems with it.
+Python 3.6 is better.
+
+#### New files added for Anything
+``not_gail\src\imitation\algorithms\anything_module.py``\
+``not_gail\src\imitation\rewards\neg_discrim_nets.py``
---
+++ test_anything_module.py
@@ -0,0 +1,30 @@
+import gym
+import os
+import numpy as np
+import pickle5 as pickle
+from imitation.data import rollout
+from imitation.util import logger, util
+from imitation.algorithms import anything_module
+from stable_baselines3 import PPO
+
+
+env = gym.make('CartPole-v1')
+
+with open("cartpole_proper.pkl", "rb") as f:
+    trajectories = pickle.load(f)
+
+transitions = rollout.flatten_trajectories(trajectories)
+
+venv = util.make_vec_env("CartPole-v1", n_envs=1)
+
+logger.configure("cartpole_neg_logs")
+
+neg_gail_trainer = anything_module.AnythingGAIL(
+    venv,
+    expert_data=transitions,
+    expert_batch_size=64,
+    gen_algo= PPO("MlpPolicy", venv, verbose=1, n_steps= int(1e3)),
+    neg_gen_algo= PPO("MlpPolicy", venv, verbose=1, n_steps= int(1e3)),
+
+)
+neg_gail_trainer.train(total_timesteps= int(1e5))
