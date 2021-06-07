cf0c7f8fba1add16ac76e12db4959c5d0bddfc7b
Shivin Dass <shivin16091@iiitd.ac.in>
2021-06-07 16:57:39 +0530

    Vae merged with main

--- modules/cnn_autoencoder.py
+++ modules/cnn_autoencoder.py
@@ -2,54 +2,37 @@ import gym
 import numpy as np
 import torch as th
 from torch import nn
+from torch.nn.utils.rnn import pad_sequence, pack_padded_sequence
 from imitation.util import networks
 from imitation.data.types import Trajectory
 from imitation.data import rollout
 
 from stable_baselines3.common import preprocessing
+from stable_baselines3.common.torch_layers import NatureCNN
 
 import torch.nn.functional as F
 
-class Flatten(nn.Module):
-    def forward(self, input):
-        return input.view(input.size(0), -1)
+class CNNAutoEncoder(nn.Module):
+    """CRNN that takes a trajectory of action, image observation pairs and returns a logit"""
 
+    def __init__(
+        self, action_space: gym.Space, observation_space: gym.Space, **mlp_kwargs
+    ):
+        super().__init__()
 
-class UnFlatten(nn.Module):
-    def __init__(self, size = 256):
-        super(UnFlatten, self).__init__()
-        self.size = size 
-    
-    def forward(self, input):
-        return input.view(input.size(0), self.size, 1, 1)
+        self.observation_space = observation_space
+        self.action_space = action_space
 
         self.feature_dim = 256
         self.encoder = NatureCNN(observation_space, features_dim=self.feature_dim)
         
 
-class CNN_VAE(nn.Module):
-    def __init__(self, image_channels=3, h_dim=256, z_dim=32):
-        super(CNN_VAE, self).__init__()
-        self.features_dim  = z_dim
-        self.encoder = nn.Sequential(
-            nn.Conv2d(image_channels, 32, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=4, stride=2),
-            nn.ReLU(),
-            nn.Conv2d(128, 256, kernel_size=4, stride=2),
-            nn.ReLU(),
-            Flatten(),
+        self.flatten_decoder = nn.Sequential(
+            
         )
 
-        self.fc1 = nn.Linear(h_dim, z_dim)
-        self.fc2 = nn.Linear(h_dim, z_dim)
-        self.fc3 = nn.Linear(z_dim, h_dim)
-
         self.decoder = nn.Sequential(
-            UnFlatten(h_dim),
-            nn.ConvTranspose2d(h_dim, 128, kernel_size=4, stride=2),
+            nn.ConvTranspose2d(self.feature_dim, 128, kernel_size=5, stride=2),
             nn.ReLU(),
             nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2),
             nn.ReLU(),
@@ -59,31 +42,17 @@ class CNN_VAE(nn.Module):
             nn.Sigmoid()
         )
 
-    # def infer_h_dim(self, obs):
-    #     self.h_dim = 
+    def _torchify_array(self, ndarray: np.ndarray, **kwargs) -> th.Tensor:
+        return th.as_tensor(ndarray, device=self.device(), **kwargs)
 
-    def reparameterize(self, mu, logvar):
-        std = logvar.mul(0.5).exp_()
-        # return torch.normal(mu, std)
-        esp = th.randn(*mu.size(), device=self.device())
-        z = mu + std * esp
-        return z
-
-    def bottleneck(self, h):
-        mu, logvar = self.fc1(h), self.fc2(h)
-        z = self.reparameterize(mu, logvar)
-        return z, mu, logvar
-
-    def encode(self, x):
-        h = self.encoder(x)
-        # print("H", h.shape)
-        z, mu, logvar = self.bottleneck(h)
-        return z, mu, logvar
-
-    def decode(self, z):
-        z = self.fc3(z)
-        z = self.decoder(z)
-        return z
+    def _torchify_with_space(
+        self, ndarray: np.ndarray, space: gym.Space, **kwargs
+    ) -> th.Tensor:
+        tensor = th.as_tensor(ndarray, device=self.device(), **kwargs)
+        preprocessed = preprocessing.preprocess_obs(
+            tensor, space, normalize_images=True,
+        )
+        return preprocessed
 
     def forward(self, input) -> th.Tensor:
         # input = self._torchify_with_space(input, self.observation_space)
@@ -94,9 +63,6 @@ class CNN_VAE(nn.Module):
         # print(encoder_vec.shape)
         return self.decoder(encoder_vec)
 
-    def forward(self, x):
-        z, _, _ = self.encode(x)
-        return z
 
     def device(self) -> th.device:
         """Heuristic to determine which device this module is on."""
